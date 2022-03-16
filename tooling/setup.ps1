5559d9ca67e67ac7fa87253e901018c6c904913f
blakeNaccarato <blake.naccarato@gmail.com>
2022-03-16 09:51:21 -0700

    Bump template

--- tooling/setup.ps1
+++ tooling/setup.ps1
@@ -1,4 +1,4 @@
-copier -f -r 16fc09d
+copier -f -r b2ba580
 Remove-Item .venv -Recurse -ErrorAction SilentlyContinue
-py -3.10 -m venv .venv
+py -3.10 -m venv .venv --upgrade-deps
 . tooling/update.ps1

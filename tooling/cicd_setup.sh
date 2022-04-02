e4d5a4f362da3948ff9918b5a71db7ba9bd7e6ea
blakeNaccarato <blake.naccarato@gmail.com>
2022-04-02 11:25:38 -0700

    Bump template

--- tooling/cicd_setup.sh
+++ tooling/cicd_setup.sh
@@ -1,6 +1,5 @@
-pip install -U pip wheel
-pip install pipx
-pipx install copier==6.0.0a9
-copier -f -r 9c653f8
+pip install -U pip setuptools wheel
+pip install git+https://github.com/copier-org/copier@master  # copier==6.0.0a9
+copier -f -r f615521
 pip install -r tooling/requirements_cicd.txt
 flit install
